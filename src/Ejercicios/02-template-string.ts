367064e97565428d345bd492c1efbf582a69e99a
Alexis Leiva <alexisleivabello072020@gmail.com>
2025-10-06 12:28:29 -0500

    Desarrollo de archivo 02-template-string.ts: Ejercicio 2

---
+++ src/Ejercicios/02-template-string.ts
@@ -0,0 +1,12 @@
+// Declaramos las variables
+let nombre: string = "Alexis";
+let carrera: string = "Ingeniería de sistemas";
+let semestre: number = 9;
+
+// Usamos un template string para mostrar un mensaje en consola
+console.log(`Hola, mi nombre es ${nombre}, estudio ${carrera} y actualmente estoy en el semestre ${semestre}.`);
+
+/*
+ Los template strings o plantillas literales se crean con los backticks (`) en lugar de comillas, lo que permiten
+ insertar variables directamente dentro del texto usando ${variable}.
+*/

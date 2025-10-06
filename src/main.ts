63ab1fa94d8e3012faf938bb97c1b6c3d04bb834
Alexis Leiva <alexisleivabello072020@gmail.com>
2025-10-06 12:41:10 -0500

    Importacion de 03-object-literal.ts en main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
 import './Ejercicios/01-variables'
 import './Ejercicios/02-template-string'
-
+import './Ejercicios/03-object-literal'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Hello Vite!</h1>  

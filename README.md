e7634cdf4b69b050d4d106d7163a791c6adc6963
Andrey Suarez <afelipesuarez@ucundinamarca.edu.co>
2025-10-06 11:45:43 -0500

    Subir ejercicio en la rama

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ### Andrey Felipe Suarez Cano
 ### Edwin Alexis Leiva Bello
 ### Brayan Fernando Jimenez Murcia
+
+# Ejercicio 7
+Este código define un arreglo llamado calificaciones con varias notas numéricas.
+Después usa la desestructuración de arreglos para obtener los dos primeros elementos y mostrarlos en la consola.
---
+++ src/Ejercicios/07-array-destructuring.ts
@@ -0,0 +1,9 @@
+// Reutilizar el arreglo de calificaciones
+const calificaciones = [4.5, 3.8, 4.9, 3.2, 4.0];
+
+// Desestructurar el primer y segundo elemento
+const [primera, segunda] = calificaciones;
+
+// Mostrar valores en consola
+console.log("Primera calificación:", primera);
+console.log("Segunda calificación:", segunda);
